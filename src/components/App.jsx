9f6cddea95f725c8433d1040c6720b944244584e
Yaroslav Vyshynskyi <92641328+YaroslavVyshynskyi@users.noreply.github.com>
2022-07-14 22:01:50 +0300

    add test file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import { Container } from "components/test/test";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <div>hello</div>
+      <Container />
     </div>
   );
 };
---
+++ src/components/test/test.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types"
+
+
+export const Container = () => {
+    return (
+        <div className="container">
+            <p className="title">Hello kitty</p>
+        </div>
+    );
+};
+
+// Container.propType { 
+
+// }
